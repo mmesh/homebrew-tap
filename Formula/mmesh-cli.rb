4cdf6d3a76e9aa269bb7785830f94eae9ccff0e5
mmeshbot <mmeshbot@mmesh.io>
2022-04-26 21:18:05 +0000

    Brew formula update for mmesh-cli version v0.1.0

--- Formula/mmesh-cli.rb
+++ Formula/mmesh-cli.rb
@@ -11,18 +11,24 @@ class MmeshCli < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/mmesh/m-cli/releases/download/v0.1.0/mmesh-cli_0.1.0_darwin_arm64.tar.gz"
-      sha256 "7d1c4dcb62352b7438c0aa7e6c81698c4a8e4d06fa602b6fbac8441f96d8621f"
+      sha256 "bb8376f0440c59a130d2fa234d94032eef2c3599e0aadd3b4c2249f432145fa3"
 
       def install
         bin.install "mmeshctl"
+        bash_completion.install "completions/mmeshctl.bash" => "mmeshctl"
+        zsh_completion.install "completions/mmeshctl.zsh" => "_mmeshctl"
+        fish_completion.install "completions/mmeshctl.fish"
       end
     end
     if Hardware::CPU.intel?
       url "https://github.com/mmesh/m-cli/releases/download/v0.1.0/mmesh-cli_0.1.0_darwin_amd64.tar.gz"
-      sha256 "c9736c68d4aa58ad1c7f56fe212394a8a06e68946aa6ed4db42d2e053f4de00e"
+      sha256 "93830db8b58e0ed71f6abf33ba98f27fddf6479648dfb9fce8d58bd6c83a5228"
 
       def install
         bin.install "mmeshctl"
+        bash_completion.install "completions/mmeshctl.bash" => "mmeshctl"
+        zsh_completion.install "completions/mmeshctl.zsh" => "_mmeshctl"
+        fish_completion.install "completions/mmeshctl.fish"
       end
     end
   end
@@ -30,19 +36,29 @@ class MmeshCli < Formula
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/mmesh/m-cli/releases/download/v0.1.0/mmesh-cli_0.1.0_linux_arm64.tar.gz"
-      sha256 "5f771688589efa51a9c09ad49a527c36952166fcf0804a63ffe2d0fdcc39708c"
+      sha256 "a56ec948fcca289c5f31906cc578f0047ed0c7b234fe8fc4887d44bd82e832d3"
 
       def install
         bin.install "mmeshctl"
+        bash_completion.install "completions/mmeshctl.bash" => "mmeshctl"
+        zsh_completion.install "completions/mmeshctl.zsh" => "_mmeshctl"
+        fish_completion.install "completions/mmeshctl.fish"
       end
     end
     if Hardware::CPU.intel?
       url "https://github.com/mmesh/m-cli/releases/download/v0.1.0/mmesh-cli_0.1.0_linux_amd64.tar.gz"
-      sha256 "de21072c7f471dbf60734c82501495f1e636556a8e9751fe4383f5392bf50220"
+      sha256 "1be0db997f8c00ff546b52b378e62fa18499c3581942c9cedd4e66b49d2ecae9"
 
       def install
         bin.install "mmeshctl"
+        bash_completion.install "completions/mmeshctl.bash" => "mmeshctl"
+        zsh_completion.install "completions/mmeshctl.zsh" => "_mmeshctl"
+        fish_completion.install "completions/mmeshctl.fish"
       end
     end
+  end
+
+  test do
+    system "#{bin}/mmeshctl version show"
   end
 end
